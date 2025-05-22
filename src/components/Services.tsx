259d7b649cf43678688685f5adea2fd1c9b58ef6
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-05-22 11:43:29 +0530

    minor change

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -9,17 +9,6 @@ const ServiceCard = ({ icon, title, description }: { icon: React.ReactElement<{ 
       </div>
       <h3 className="text-xl font-bold text-gray-800 mb-3">{title}</h3>
       <p className="text-gray-600">{description}</p>
-      <div className="mt-4">
-        <a
-          href="#contact"
-          className="text-[#005DA6] font-medium hover:text-[#F7CB05] transition-all duration-300 inline-flex items-center"
-        >
-          Learn More
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
-          </svg>
-        </a>
-      </div>
     </div>
   );
 };
